8db0d70df7209baaf9e415921992f75cce4b8016
Marissa Shey <shey@tuta.io>
2020-05-10 21:36:43 -0400

    create the basic components
    (html, css, and javascript files for basic page animation, and a manifest.json file for the chrome extension)

---
+++ scripts.js
@@ -0,0 +1,27 @@
+
+const input = document.getElementById('input');
+const tailHolder = document.getElementById('tailHolder');
+
+input.addEventListener('input', function(e) {
+  const {value} = this;
+
+  if (value.includes('.')) {
+    cretaeTail(value);
+    this.value='';
+    
+  }
+  
+});
+
+function cretaeTail(text) {
+  const tail = document.createElement('div');
+  tail.classList.add('gravity');
+  tail.innerHTML = text;
+  
+  tailHolder.append(tail);
+  setTimeout(() => tail.remove(), 1000);
+  requestAnimationFrame(() => {
+    tail.classList.add('fall-right');
+  });
+}
+sad
